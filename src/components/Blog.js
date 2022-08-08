5fd9422898b873a08b47a0ea5a7a5df3fe37893c
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2022-08-08 13:29:34 +0300

    Task 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+  const hidden = { display: visible ? "" : "none" };
 
-export default Blog
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <article style={blogStyle}>
+      {blog.title} {blog.author} {""}
+      {!visible ? (
+        <button onClick={toggleVisibility}>Expand</button>
+      ) : (
+        <button onClick={toggleVisibility}>Collapse</button>
+      )}
+      <div style={hidden}>
+        <a href={blog.url}>{blog.url}</a>
+        <br></br>
+        {blog.likes} likes <button>Like</button>
+        <br></br>
+        {blog.user.name}
+      </div>
+    </article>
+  );
+};
+
+export default Blog;
